4f35821edd6301ddf7b871db68c1ad316c07a6e4
zbldsg <zbldsg@163.com>
2023-04-17 12:32:15 +0800

    去除掉无用代码

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # SimpleVue
 Write a simple Vue framework by yourself and implement its reactivity feature !
+
+
+这个简单的响应式JavaScript框架实现了以下功能：
+
+- 观察者模式：Observer类定义了一个对象的观察者，可以在属性值改变时通知订阅者，订阅者可以更新视图。
+- 发布订阅模式：Dep类作为一个发布者，维护一个订阅者数组，可以增加或删除订阅者，通知所有订阅者更新视图。
+- 监听属性变化：defineReactive方法使用Object.defineProperty实现属性的get和set方法，使得当属性值变化时可以通知订阅者更新视图。
+- 计算属性：Watcher类的构造函数可以接收一个函数作为第二个参数，该函数可以计算出一个新值，然后在属性改变时更新视图。
+- 指令：Compiler类根据指令类型处理指令，例如v-bind、v-model和v-if等指令。
+- 双向绑定：对于v-model指令，实现了双向数据绑定，当表单元素的值改变时，会同步更新数据模型，反之亦然。
+- 模板编译：Compiler类使用递归算法编译模板，将模板中的表达式解析为JavaScript代码，并生成一个更新视图的函数，并将该函数添加到订阅者数组中，从而实现响应式的视图更新。
--- vue.js
+++ vue.js
@@ -22,6 +22,7 @@ class Observer {
             enumerable: true,
             configurable: true,
             get: function () {
+                // 当该属性被读取时，如果当前有正在执行的 watcher，那么将该 watcher 添加到属性的订阅器 dep 中，以便于在属性变化时更新该 watcher
                 if (Dep.target) {
                     dep.depend();
                 }
@@ -39,22 +40,37 @@ class Observer {
     }
 }
 
+
+/**
+ * 在Vue中，dep是依赖收集器的简称，其作用是在数据对象和Watcher对象之间建立联系，
+ * 用于跟踪哪些组件依赖于哪些数据，以便在数据发生变化时，能够通知到所有依赖于该数据的组件进行相应的更新操作。
+ * Vue中的dep更新机制是基于观察者模式实现的，其实现过程如下：
+
+ 在组件的render()函数中，访问数据对象的属性时，会触发该属性的get()方法；
+ 在get()方法中，会将当前的Watcher对象添加到该属性的依赖收集器（即dep对象）的订阅列表中；
+ 在组件销毁时，会将该组件中所有的Watcher对象从其依赖的数据对象中的所有dep对象的订阅列表中移除；
+ 当数据对象的属性发生变化时，会触发该属性的set()方法；
+ 在set()方法中，会遍历该属性的依赖收集器中所有的Watcher对象，并调用它们的update()方法，以通知所有依赖于该属性的组件进行更新操作。
+ 简单来说，Vue的dep更新机制主要有两个关键步骤：收集依赖和通知更新。在收集依赖阶段中，Vue会在组件的render()函数中访问数据对象的属性时，将当前的Watcher对象添加到该属性的依赖收集器中，从而建立数据对象和Watcher对象之间的联系；在通知更新阶段中，Vue会在数据对象的属性发生变化时，遍历该属性的依赖收集器中所有的Watcher对象，并调用它们的update()方法，从而通知所有依赖于该属性的组件进行更新操作。
+
+ 通过dep更新机制，Vue能够高效地跟踪组件之间的数据依赖关系，从而实现组件的实时响应和更新。
+ */
 // 订阅器，用于收集 watcher，并在属性变化时通知 watcher 更新
 class Dep {
     constructor() {
-        this.subs = [];
+        this.subscribers = [];
     }
 
     // 添加一个 watcher
-    addSub(sub) {
-        this.subs.push(sub)
+    addSubscriber(subscriber) {
+        this.subscribers.push(subscriber)
     }
 
     // 移除一个 watcher
-    removeSub(sub) {
-        let index = this.subs.indexOf(sub);
+    removeSubscriber(subscriber) {
+        let index = this.subscribers.indexOf(subscriber);
         if (index > -1) {
-            this.subs.splice(index, 1)
+            this.subscribers.splice(index, 1)
         }
     }
 
@@ -63,79 +79,16 @@ class Dep {
         Dep.target.addDep(this);
     }
 
+    // 通知所有已订阅该属性变化的 watcher 更新
     notify() {
-        this.subs.forEach((sub) => {
-            sub.update()
+        this.subscribers.forEach((subscriber) => {
+            subscriber.update()
         })
     }
-
 }
 
 // 全局唯一的正在执行的 watcher，用于在属性被读取时收集订阅器 dep
 Dep.target = null
-
-// 定义观察者类，用于将依赖的数据绑定到回调函数中
-class Watcher {
-    constructor(vm, expOrFn, cb) {
-        this.vm = vm; // Vue实例
-        this.expOrFn = expOrFn; // 表达式或函数
-        this.cb = cb; // 回调函数
-        this.depIds = {}; // 依赖的ID列表
-
-        // 根据表达式或函数设置getter
-        if (typeof expOrFn === "function") {
-            this.getter = expOrFn;
-        } else {
-            this.getter = this.parseGetter(expOrFn);
-        }
-
-        // 保存获取到的值
-        this.value = this.get();
-    }
-
-    // 添加依赖
-    addDep(dep) {
-        // 如果依赖的ID不在依赖的ID列表中，则将该依赖添加到列表中
-        if (!this.depIds.hasOwnProperty(dep.id)) {
-            dep.addSub(this); // 添加观察者到依赖中
-            this.depIds[dep.id] = dep; // 将依赖的ID添加到依赖的ID列表中
-        }
-    }
-
-    // 获取值
-    get() {
-        // 设置Dep.target为当前观察者
-        Dep.target = this;
-        // 通过调用getter获取数据
-        // const value = this.getter.call(this.vm, this.vm);
-        const value = this.vm.data[this.expOrFn]
-        // 将Dep.target设置为null
-        Dep.target = null;
-        // 返回获取到的值
-        return value;
-    }
-
-    update() {
-        let oldValue = this.value;
-        this.value = this.get(); // 获取新的值
-        // 调用回调函数，并传入新旧值
-        this.cb.call(this.vm, this.value, oldValue);
-    }
-
-    // 解析getter
-    parseGetter(exp) {
-        const segments = exp.split(".");
-        return function (obj) {
-            for (let i = 0; i < segments.length; i++) {
-                if (!obj) {
-                    return;
-                }
-                obj = obj[segments[i]];
-            }
-            return obj;
-        };
-    }
-}
 
 class Compiler {
     // 构造函数，接收一个参数 vm，代表 Vue 实例
@@ -149,9 +102,9 @@ class Compiler {
     }
 
     compile(node) {
-        console.log(node, node.nodeType, 'node................')
         if (node.nodeType === 1) {//是元素节点
             this.compileElement(node)//解析节点上的属性
+            //遍历该元素的所有子元素,并递归调用
             node.childNodes.forEach(childNode => {
                 this.compile(childNode)
             })
@@ -160,10 +113,11 @@ class Compiler {
         }
     }
 
+    // 定义 compileElement 方法，接收一个参数 node，代表当前处理的元素节点
     compileElement(node) {
+        // 获取当前元素节点的所有属性
         const attrs = node.attributes;
-        console.log(attrs, '属性')
-        console.log(Array.from(attrs), '数组')
+        // 将 attrs 类数组转为数组，遍历数组中的每个属性
         Array.from(attrs).forEach((attr) => {
             // 获取属性名和属性值
             const attrName = attr.name;
@@ -174,7 +128,7 @@ class Compiler {
                 const dir = attrName.substring(2);
                 // 根据指令名调用相应的方法进行处理
                 if (dir === "bind") {
-                    // this.bindText(node, attrValue);
+                    this.bind(node, attrValue);
                 } else if (dir === "model") {
                     this.bindModel(node, attrValue);
                 } else if (dir === "for") {
@@ -187,27 +141,41 @@ class Compiler {
         })
     }
 
-    bindText(node, exp) {
+    bind(node, exp) {
+
+    }
+
+    bindText(node, exp, key) {
+        //将表达式替换为data里的变量值
+        node.nodeValue = node.nodeValue.replace(exp, this.vm.data[key])
+
+        // 定义 updateFn 函数，用于更新文本节点的内容
         const updateFn = (value) => {
             node.textContent = value;
         };
-        new Watcher(this.vm, exp, updateFn);
+        // 创建 Watcher 实例，传入 vm、key 和 updateFn 三个参数
+        new Watcher(this.vm, key, updateFn);
     }
 
+    // 定义 compileText 方法，接收一个参数 node，代表当前处理的文本节点
     compileText(node) {
+        //正则: 用来匹配包含在两个花括号之间的任意字符
         let reg = /{{(.*)}}/;
+
+        // 如果文本内容中包含插值表达式，则获取表达式，并调用 bindText 方法进行处理
         if (reg.test(node.nodeValue)) {
+            //exec() 方法返回的数组包含匹配到的子串以及任何捕获组中捕获到的子串的信息。
+            // 返回的数组的第一个元素是匹配到的完整文本，
+            // 而后续元素则是由正则表达式中定义的捕获组所匹配到的文本
             let match = reg.exec(node.nodeValue)
             const exp = match[0] //带双括号的变量名
             const key = match[1].trim(); //不带双括号的变量名
-            //将表达式替换为data里的变量值
-            node.nodeValue = node.nodeValue.replace(exp, this.vm.data[key])
-
-            this.bindText(node, key)
+            this.bindText(node, exp, key)
         }
     }
 
     bindModel(node, exp) {  // 处理 'v-model' 指令
+        //将v-model 所在dom元素的值改为 v-model 绑定的data中数据的值
         node.value = this.vm.data[exp];
 
         const updateFn = (value) => {  // 定义更新视图的函数
@@ -243,13 +211,54 @@ class Compiler {
         parentNode.insertBefore(placeholder, node);
 
         // 获取data中的数值, 如果是false 直接删除节点
-        if(!this.vm.data[exp]) {
+        if (!this.vm.data[exp]) {
             parentNode.removeChild(node);
         }
         // 创建一个新的 Watcher 实例，监听表达式的变化，并在变化时调用 updateFn 方法更新 DOM
         new Watcher(this.vm, exp, updateFn);
     }
 
+}
+
+// 定义观察者类，用于将依赖的数据绑定到回调函数中
+class Watcher {
+    constructor(vm, expOrFn, callBack) {
+        this.vm = vm; // Vue实例
+        this.expOrFn = expOrFn; // 表达式或函数
+        this.callBack = callBack; // 回调函数
+        this.depIds = {}; // 依赖的ID列表
+
+        // 保存获取到的值
+        this.value = this.get();
+    }
+
+    // 添加依赖
+    addDep(dep) {
+        // 如果依赖的ID不在依赖的ID列表中，则将该依赖添加到列表中
+        if (!this.depIds.hasOwnProperty(dep.id)) {
+            dep.addSubscriber(this); // 添加观察者到依赖中
+            this.depIds[dep.id] = dep; // 将依赖的ID添加到依赖的ID列表中
+        }
+    }
+
+    // 获取值
+    get() {
+        // 设置Dep.target为当前观察者
+        Dep.target = this;
+        // 通过调用getter获取数据
+        const value = this.vm.data[this.expOrFn]
+        // 将Dep.target设置为null
+        Dep.target = null;
+        // 返回获取到的值
+        return value;
+    }
+
+    update() {
+        let oldValue = this.value;
+        this.value = this.get(); // 获取新的值
+        // 调用回调函数，并传入新旧值
+        this.callBack.call(this.vm, this.value, oldValue);
+    }
 }
 
 class Vue {
